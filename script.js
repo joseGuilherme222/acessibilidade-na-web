d4d60223ecfdab760cf9d86e57983482c000b2cf
joseGuilherme222 <150289750+joseGuilherme222@users.noreply.github.com>
2025-02-27 13:33:57 -0300

    .

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-
+    const diminuir FonteBotao = document.getElementById('diminuir-fonte');
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
     });
+    dminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+
+
 });
