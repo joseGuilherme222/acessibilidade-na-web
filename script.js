98c3182428366af4a973b2eadf7c7f3c251eeaba
joseGuilherme222 <150289750+joseGuilherme222@users.noreply.github.com>
2025-04-01 13:25:49 -0300

    .

--- script.js
+++ script.js
@@ -17,3 +17,6 @@ document.addEventListener('DOMContentLoaded', function(){
 
 })
 const alternaContraste = document.getElementById('alterna-contraste')
+alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
+})
