d3492d5dd4780112ac6c4fffe8fe04c2f0fe7bd5
joseGuilherme222 <150289750+joseGuilherme222@users.noreply.github.com>
2025-04-01 13:29:20 -0300

    .

--- script.js
+++ script.js
@@ -21,3 +21,4 @@ alternaContraste.addEventListener('click', function(){
     document.body.classList.toggle('alto-contraste')
 })
 const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
